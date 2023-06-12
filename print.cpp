b79844bc54862b6c3240965050e40278a4873962
Malak-Moustafa <malakahmedmoustafa@aucegypt.edu>
2023-06-12 21:33:54 +0300

    commit 1

---
+++ print.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+void printBoard(int board[N][N]) {
+   for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++)
+         cout << board[i][j] << " ";
+         cout << endl;
+   }
+}
